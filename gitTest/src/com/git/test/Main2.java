76a523b0bd452a7088f84cb7cbad1c10598ae798
user1 <user1@user1-PC>
2018-12-19 14:54:59 +0900

    main2 address 추가

--- gitTest/src/com/git/test/Main2.java
+++ gitTest/src/com/git/test/Main2.java
@@ -6,6 +6,12 @@ public class Main2 {
 	private String name;
 	private String hobby;
 	
+	private String address;
+	
+	public String getAddress() {
+		return address;
+	}
+	
 	public String getName() {
 		return name;
 	}
