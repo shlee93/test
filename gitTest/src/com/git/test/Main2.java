047c54d1db3f821e0c298b77f6f53d9c95aa60f9
user1 <user1@user1-PC>
2018-12-19 14:40:52 +0900

    모르겠다.... 일단 getName 추가

--- gitTest/src/com/git/test/Main2.java
+++ gitTest/src/com/git/test/Main2.java
@@ -3,8 +3,12 @@ package com.git.test;
 public class Main2 {
 
 	private int age;
+	private String name;
 	private String hobby;
 	
+	public String getName() {
+		return name;
+	}
 	public String getHobby() {
 		return hobby;
 	}
